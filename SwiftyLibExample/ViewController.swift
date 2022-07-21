d43eb99f8105c4e4d060af4e7fcba6838b0ea85e
Nick Huang <nick25932219@gmail.com>
2022-07-21 16:53:07 +0800

    Initial Commit

---
+++ SwiftyLibExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftyLibExample
+//
+//  Created by NickHuang on 2022/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
